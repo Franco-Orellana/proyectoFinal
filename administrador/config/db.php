2e5d6170d5ae142a2bb6238fca5d3a0c72ae43fd
Franco-Orellana <97976446+Franco-Orellana@users.noreply.github.com>
2023-06-04 17:38:26 -0300

    Update db.php

--- administrador/config/db.php
+++ administrador/config/db.php
@@ -1,8 +1,10 @@
 <?php 
-    $host = "localhost";
+    $host = "containers-us-west-165.railway.app";
     $database = "inmobiliaria";
     $usuario = "root";
-    $contrasenia = "";
+    $contrasenia = "e0WuMthO3MAc9wJcU5ge";
+    $port = "6608";
+    $dbname ="railway";
 
     try {
         $conexion = new PDO("mysql:host=$host;dbname=$database", $usuario, $contrasenia);
